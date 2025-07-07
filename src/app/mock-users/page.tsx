d6f3cbcceccd7f93df7ff4fbd69acee4f47a7d6e
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-07-07 12:57:37 -0700

    Practicing server action through submission form with mockapi

---
+++ src/app/mock-users/page.tsx
@@ -0,0 +1,47 @@
+import { revalidatePath } from "next/cache";
+
+type MockUser = {
+  id: number;
+  name: string;
+};
+
+export default async function MockUsers() {
+  const res = await fetch("https://686c15fc14219674dcc733a8.mockapi.io/users");
+  const users = await res.json();
+
+  // server action
+  async function addUser(formData: FormData) {
+    "use server"
+    const name = formData.get('name')
+    const res = await fetch("https://686c15fc14219674dcc733a8.mockapi.io/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+    })
+    const newUser = await res.json();
+    // updates UI on form submit without having to refresh
+    revalidatePath("/mock-users")
+    console.log(newUser)
+  }
+
+  return (
+    <div className="py-10">
+        <form action={addUser} className="mb-4">
+            <input type="text" name="name" required className="border p-2 mr-2 "/>
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Add User</button>
+        </form>
+      <div className="grid grid-cols-4 gap-4 py-10">
+        {users.map((user: MockUser) => (
+          <div
+            key={user.id}
+            className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+          >
+            {user.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
